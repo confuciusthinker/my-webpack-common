754da41b12f7571e3311e1553869a23df4b0fbb8
confuciusthinker <1006398378@qq.com>
2022-12-08 22:06:17 +0800

    feat: 完成webpack5通用配置

---
+++ babel.config.js
@@ -0,0 +1,33 @@
+module.exports = { 
+    presets: [
+        [
+            "@babel/preset-env",  //ES的新语法转译
+            {
+                useBuiltIns: 'entry',
+                corejs: 3,
+            }   
+        ],
+        "@babel/preset-react",
+        "@babel/preset-typescript",
+    ],
+    plugins: [
+        [
+            "@babel/plugin-transform-runtime",
+            {
+                corejs: false,
+                helpers: false,
+            }
+        ], //处理polyfill
+        "@babel/plugin-syntax-dynamic-import", //处理懒加载
+        "lodash",
+        [
+            'import',
+            {
+                libraryName: 'antd',
+                libraryDirectory: 'es',
+                style: true,
+            },
+            'antd'
+        ] //antd组件按需加载
+    ]
+}
